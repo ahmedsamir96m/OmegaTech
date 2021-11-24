682a80ebd18b3241706b085979fb121477a7a678
ahmedsamir96m <ahmedsamir96m.com>
2021-11-24 23:04:24 +0200

    created Navbar component folder, Navbar.js, styles.js, created logo folder in assets, added the main logo picture, created the basic logic and styles for the navigation bar , logo and the shopping cart

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core'
+import { ShoppingCart } from '@material-ui/icons'
+
+import useStyles from './styles'
+import logo from '../../assets/logo/omega-logo.jpg'
+
+const Navbar = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography>
+            <img src={logo} alt="Omega Tech" height="25px" className={classes.image} />
+            OMEGA
+          </Typography>
+          <div className={classes.grow}></div>{/* for spacing purposes */}
+          <div className={classes.button}>
+            <IconButton aria-label="Show Cart Items" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart>
+                </ShoppingCart>
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  )
+}
+
+export default Navbar
